375aefa5c1bf0382801a12795b619da87ec20aa4
PeriklisCharalampidis <periklis.charalampidis@hotmail.com>
2023-12-13 13:32:40 +0100

    EasyAdm

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Activities;
+use App\Entity\JournalPost;
+use App\Entity\PakingList;
+use App\Entity\Trip;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin', name: 'admin_')]
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/', name: 'admin')]
+    public function index(): Response
+    {
+        # return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        return $this->render('admin/index.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('TripRoamPlanner');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Users', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Packing Items   ', 'fas fa-list', PakingList::class);
+        yield MenuItem::linkToCrud('Activities', 'fas fa-list', Activities::class);
+        yield MenuItem::linkToCrud('JournalPost', 'fas fa-list', JournalPost::class);
+        yield MenuItem::linkToCrud('Trip', 'fas fa-list', Trip::class);
+    }
+}
---
+++ src/Controller/Admin/JournalPostCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\JournalPost;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class JournalPostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return JournalPost::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
