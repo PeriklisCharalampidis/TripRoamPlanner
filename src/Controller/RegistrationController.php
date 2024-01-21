5661ad0bf8a19554aee9839e66324870d68d46fc
Viktoria Fedorova <viktoriia.fedorova1@gmail.com>
2024-01-21 20:06:56 +0100

    added .htacess file for deploying

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -21,7 +21,7 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
+
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -31,7 +31,6 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_login');
         }
